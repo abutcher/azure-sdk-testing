e7e5baec1c2ebf50407eecc829956b583d810739
Andrew Butcher <abutcher@redhat.com>
2022-08-26 11:49:07 -0400

    Delete the private zone instead of the resource group.

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/privatedns/armprivatedns"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
 )
 
 var (
@@ -51,8 +50,7 @@ func main() {
 	}
 
 	// Select user-assigned identity via its clientID.
-	// Does the clientID come from a secret? OR do we create a NewManagedIdentityCredential
-	// with no options to select a system-assigned identity instead?
+	// Does the clientID come from a secret?
 	clientID := azidentity.ClientID(clientID)
 	opts := azidentity.ManagedIdentityCredentialOptions{ID: clientID}
 	cred, err := azidentity.NewManagedIdentityCredential(&opts)
@@ -61,27 +59,11 @@ func main() {
 	}
 	ctx := context.Background()
 
-	/*
-		resourceGroup, err := createResourceGroup(ctx, cred)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println("resources group:", *resourceGroup.ID)
-	*/
-
 	privateZone, err := createPrivateZone(ctx, cred)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Println("private zone:", *privateZone.ID)
-
-	/*
-		recordSets, err := createRecordSets(ctx, cred)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println("record sets:", *recordSets.ID)
-	*/
 
 	keepResource := os.Getenv("KEEP_RESOURCE")
 	if len(keepResource) == 0 {
@@ -118,62 +100,20 @@ func createPrivateZone(ctx context.Context, cred azcore.TokenCredential) (*armpr
 	return &resp.PrivateZone, nil
 }
 
-func createRecordSets(ctx context.Context, cred azcore.TokenCredential) (*armprivatedns.RecordSet, error) {
-	recordSets, err := armprivatedns.NewRecordSetsClient(subscriptionID, cred, nil)
+func cleanup(ctx context.Context, cred azcore.TokenCredential) error {
+	privateZonesClient, err := armprivatedns.NewPrivateZonesClient(subscriptionID, cred, nil)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	resp, err := recordSets.CreateOrUpdate(
+	pollersResp, err := privateZonesClient.BeginDelete(
 		ctx,
 		resourceGroupName,
 		privateZoneName,
-		armprivatedns.RecordTypeA,
-		relativeRecordSetName,
-		armprivatedns.RecordSet{
-			Properties: &armprivatedns.RecordSetProperties{
-				ARecords: []*armprivatedns.ARecord{},
-			},
-		},
 		nil,
 	)
-	if err != nil {
-		return nil, err
-	}
-	return &resp.RecordSet, nil
-}
 
-func createResourceGroup(ctx context.Context, cred azcore.TokenCredential) (*armresources.ResourceGroup, error) {
-	resourceGroupClient, err := armresources.NewResourceGroupsClient(subscriptionID, cred, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	resourceGroupResp, err := resourceGroupClient.CreateOrUpdate(
-		ctx,
-		resourceGroupName,
-		armresources.ResourceGroup{
-			Location: to.Ptr(location),
-		},
-		nil)
-	if err != nil {
-		return nil, err
-	}
-	return &resourceGroupResp.ResourceGroup, nil
-}
-
-func cleanup(ctx context.Context, cred azcore.TokenCredential) error {
-	resourceGroupClient, err := armresources.NewResourceGroupsClient(subscriptionID, cred, nil)
-	if err != nil {
-		return err
-	}
-
-	pollerResp, err := resourceGroupClient.BeginDelete(ctx, resourceGroupName, nil)
-	if err != nil {
-		return err
-	}
-
-	_, err = pollerResp.PollUntilDone(ctx, nil)
+	_, err = pollersResp.PollUntilDone(ctx, nil)
 	if err != nil {
 		return err
 	}
